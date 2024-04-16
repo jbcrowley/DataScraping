77b51027c4459b4cf1e7d794fd77006c2a6e7c27
Jeff Crowley <jbcrowley@hotmail.com>
2024-04-15 22:28:54 -0500

    Adding ExportPunsByCounty

--- DataScraping/PageObjects/Oklahoma/PublicPunSearchPage.cs
+++ DataScraping/PageObjects/Oklahoma/PublicPunSearchPage.cs
@@ -1,9 +1,16 @@
-﻿using OpenQA.Selenium;
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Text;
 
 namespace SeleniumFramework.PageObjects.TheInternet
 {
     class PublicPunSearchPage : BasePage
     {
+        private readonly By _countySearchDropdownLocator = By.XPath("//tr[.//span[text()='Search By County:']]//select");
+        private readonly By _countySearchInputLocator = By.XPath("//tr[.//span[text()='Search By County:']]//input");
+        private readonly By _punResultsLocator = By.XPath("(//table[contains(@class,'DocTable')][.//thead//th[.='PUN'][not(contains(@class,'TVCH'))]])[1]/tbody/tr/td[1]");
         private readonly By _punSearchInputLocator = By.XPath("//tr[.//span[text()='Search By PUN:']]//input");
         private readonly By _searchButtonLocator = By.XPath("//span[text()='Search']");
 
@@ -18,6 +25,116 @@ namespace SeleniumFramework.PageObjects.TheInternet
         public void ClickPunLink(string pun)
         {
             Click(By.XPath($"//a[text()='{pun}']"));
+        }
+
+        /// <summary>
+        /// Search by county.
+        /// </summary>
+        /// <param name="countyIndex">The county SELECT index for the desired county.</param>
+        public void ExportPunsByCounty(int countyIndex)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.WriteStartObject();
+
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+                DateTime expire = DateTime.Now.AddSeconds(10);
+                IReadOnlyCollection<IWebElement> inputs = FindElements(_countySearchInputLocator);
+                while (inputs.Count == 0 && DateTime.Now < expire)
+                {
+                    inputs = FindElements(_countySearchInputLocator);
+                    Thread.Sleep(500);
+                }
+                inputs.ElementAt(0).Click();
+
+                SelectElement select = new SelectElement(FindElement(_countySearchDropdownLocator));
+                select.SelectByIndex(countyIndex);
+                string countyName = select.SelectedOption.Text;
+                writer.WritePropertyName(countyName);
+                Console.WriteLine(countyName);
+                Click(_searchButtonLocator);
+                wait.Until(ExpectedConditions.StalenessOf(inputs.ElementAt(0)));
+
+                // add loop for pages
+                IReadOnlyCollection<IWebElement> punResults = FindElements(ExpectedConditions.VisibilityOfAllElementsLocatedBy(_punResultsLocator));
+                writer.WriteValue($"{punResults.Count}");
+                Console.WriteLine($"count: {punResults.Count}");
+                writer.WritePropertyName("PUNs");
+                writer.WriteStartArray();
+                punResults.ToList().ForEach(e => writer.WriteValue(e.Text));
+                //foreach (IWebElement pun in punResults)
+                //{
+                //    writer.WriteValue(pun.Text);
+                //}
+                // writer.WriteValue(string.Join(",", punResults.Select(e => e.Text)));
+                Console.WriteLine(string.Join(",", punResults.Select(e => e.Text)));
+                writer.WriteEndArray();
+                writer.WriteEndObject();
+                Console.WriteLine(sb.ToString());
+                string filePath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory) + $@"\Output\";
+                Directory.CreateDirectory(filePath);
+                using (StreamWriter output = new StreamWriter(Path.Combine(filePath, $"{countyName}.json")))
+                {
+                    output.WriteLine(sb.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Search by county.
+        /// </summary>
+        public void ExportPunsByCounty_old()
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.WriteStartObject();
+
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+                DateTime expire = DateTime.Now.AddSeconds(10);
+                IReadOnlyCollection<IWebElement> inputs = FindElements(_countySearchInputLocator);
+                while (inputs.Count == 0 && DateTime.Now < expire)
+                {
+                    inputs = FindElements(_countySearchInputLocator);
+                    Thread.Sleep(500);
+                }
+                inputs.ElementAt(0).Click();
+
+                SelectElement select = new SelectElement(FindElement(_countySearchDropdownLocator));
+                for (int i = 1; i < 5; i++)
+                // for (int i = 1; i < select.Options.Count; i++)
+                {
+                    select.SelectByIndex(i);
+                    writer.WritePropertyName(select.SelectedOption.Text);
+                    Console.WriteLine(select.SelectedOption.Text);
+                    var punResults = FindElements(_punResultsLocator);
+                    Click(_searchButtonLocator);
+                    wait.Until(ExpectedConditions.StalenessOf(i == 1 ? inputs.ElementAt(0) : punResults.ElementAt(0)));
+                    // add loop for pages
+                    select = new SelectElement(FindElement(ExpectedConditions.ElementIsVisible(_countySearchDropdownLocator)));
+                    punResults = FindElements(ExpectedConditions.VisibilityOfAllElementsLocatedBy(_punResultsLocator));
+                    writer.WriteValue($"{punResults.Count}");
+                    Console.WriteLine($"count: {punResults.Count}");
+                    writer.WritePropertyName("PUNs");
+                    writer.WriteStartArray();
+                    foreach (IWebElement pun in punResults)
+                    {
+                        writer.WriteValue(pun.Text);
+                    }
+                    // writer.WriteValue(string.Join(",", punResults.Select(e => e.Text)));
+                    Console.WriteLine(string.Join(",", punResults.Select(e => e.Text)));
+                    writer.WriteEndArray();
+                }
+                writer.WriteEndObject();
+                Console.WriteLine(sb.ToString());
+            }
         }
 
         /// <summary>
---
+++ DataScraping/Tests/ExportPunsByCounty.cs
@@ -0,0 +1,36 @@
+﻿using SeleniumFramework.PageObjects.TheInternet;
+using SeleniumFramework.Tests;
+
+namespace DataScraping.Tests
+{
+    [Parallelizable(ParallelScope.Children)]
+    public class ExportPunsByCounty : BaseTest
+    {
+        [Test]
+        [Category("Oklahoma")]
+        [TestCaseSource("GetData")]
+        public void ExportPunsByCountyOk(int countyIndex)
+        {
+            Driver.Value!.Url = "https://oktap.tax.ok.gov/OkTAP/web?link=PUBLICPUNLKP";
+
+            PublicPunSearchPage publicPunSearchPage = new PublicPunSearchPage(Driver.Value!);
+            publicPunSearchPage.ExportPunsByCounty(countyIndex);
+            // write output to file, TBD
+        }
+
+        /// <summary>
+        /// Gets the data from the specified CSV for the data driven test.
+        /// </summary>
+        /// <returns>The test data.</returns>
+        private static IEnumerable<int> GetData()
+        {
+            // i must start at 1 to avoid the "Required" OPTION
+            // int counties = 5;
+            int counties = 78;
+            for (int i = 1; i < counties; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
